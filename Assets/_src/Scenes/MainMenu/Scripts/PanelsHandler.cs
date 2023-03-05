7e2182429fbcace294aa4f60d0b9b31026f958d1
Danil Kuzychenko <lymaaaan@gmail.com>
2023-03-05 14:47:06 +0400

    Feat: MainMenu UI was added

---
+++ Assets/_src/Scenes/MainMenu/Scripts/PanelsHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _panels;
+
+    private void OnEnable()
+    {
+        ShowPanel("MainPanel");
+    }  
+
+    private void ShowPanel(string NameOfPanelThatShouldBeShown)
+    {
+        for(int index = 0; index < _panels.Length; index++)
+        {
+            if(_panels[index].gameObject.name == NameOfPanelThatShouldBeShown)
+            {
+                _panels[index].Activate();
+            }
+            else
+            {
+                HidePanel(_panels[index]);
+            }
+        }
+    }
+
+    private void HidePanel(GameObject panel)
+    {
+        panel.Deactivate();
+    }
+
+    public void BackToMainMenu()
+    {
+        ShowPanel("MainPanel");
+    }
+
+    public void OpenSettingsPanel()
+    {
+        ShowPanel("SettingsPanel");
+    }
+
+    public void OpenLevelsPanel()
+    {
+        ShowPanel("LevelsPanel");
+    }
+}
